def587a302a3a33a27e8958256d915b12abc4167
Ülla <90785712+artistulla@users.noreply.github.com>
2021-09-24 01:02:17 +0200

    Delete app directory

--- app/NFTs/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class NftsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'NFTs'
--- app/README.md
+++
@@ -1,3 +0,0 @@
-# The App
-
-This folder contains an local web application for displaying tokens inside publicly trusted smart contracts.
